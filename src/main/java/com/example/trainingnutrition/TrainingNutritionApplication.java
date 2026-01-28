c9c957137eb6f3b4091fd0e3065387f9b134ae5c
Maru <pdijsj2992@gmail.com>
2026-01-28 19:05:27 +0300

    Initial commit: basic nutrition service structure

---
+++ src/main/java/com/example/trainingnutrition/TrainingNutritionApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingnutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingNutritionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingNutritionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trainingnutrition/TrainingNutritionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingnutrition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingNutritionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
