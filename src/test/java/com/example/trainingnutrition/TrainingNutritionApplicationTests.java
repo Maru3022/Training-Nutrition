f682baa13c277e888a557c67f5ef58db2da89000
Maru <pdijsj2992@gmail.com>
2026-01-30 21:00:42 +0300

    update TrainingNutritionApplicationTests

--- src/test/java/com/example/trainingnutrition/TrainingNutritionApplicationTests.java
+++ src/test/java/com/example/trainingnutrition/TrainingNutritionApplicationTests.java
@@ -1,15 +1,10 @@
 package com.example.trainingnutrition;
 
-import com.example.trainingnutrition.Service.messaging.KafkaProducerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class TrainingNutritionApplicationTests {
-
-    @MockitoBean
-    private KafkaProducerService kafkaProducerService;
 
     @Test
     void contextLoads() {}
