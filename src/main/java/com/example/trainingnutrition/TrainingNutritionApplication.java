eb4f61b35dd6ad248ad6200e0ab207782cd105df
Maru <pdijsj2992@gmail.com>
2026-01-30 15:28:57 +0300

    update main.yml

--- src/main/java/com/example/trainingnutrition/TrainingNutritionApplication.java
+++ src/main/java/com/example/trainingnutrition/TrainingNutritionApplication.java
@@ -1,34 +1,18 @@
 package com.example.trainingnutrition;
 
-import com.example.trainingnutrition.Repository.NutritionTipSearchRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
-@EnableJpaRepositories(
-        basePackages = "com.example.trainingnutrition.Repository",
-        includeFilters = @ComponentScan.Filter(
-                type = FilterType.ASSIGNABLE_TYPE,
-                classes = NutritionTipSearchRepository.class
-        )
-)
-@EnableElasticsearchRepositories(
-        basePackages = "com.example.trainingnutrition.Repository",
-        includeFilters = @ComponentScan.Filter(
-                type = FilterType.ASSIGNABLE_TYPE,
-                classes = NutritionTipSearchRepository.class
-        )
-)
+@EnableJpaRepositories(basePackages = "com.example.trainingnutrition.Repository")
+@EnableElasticsearchRepositories(basePackages = "com.example.trainingnutrition.Repository")
 public class TrainingNutritionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TrainingNutritionApplication.class, args);
     }
-
 }
