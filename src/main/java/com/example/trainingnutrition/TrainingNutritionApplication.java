b833752fbd87f6d1047fab4cf26fc80722af5135
Maru <pdijsj2992@gmail.com>
2026-01-30 15:14:52 +0300

    update TrainingNutritionApplication

--- src/main/java/com/example/trainingnutrition/TrainingNutritionApplication.java
+++ src/main/java/com/example/trainingnutrition/TrainingNutritionApplication.java
@@ -2,10 +2,12 @@ package com.example.trainingnutrition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableCaching
 @EnableJpaRepositories(basePackages = "com.example.trainingnutrition.Repository")
 @EnableElasticsearchRepositories(basePackages = "com.example.trainingnutrition.Repository")
 public class TrainingNutritionApplication {
