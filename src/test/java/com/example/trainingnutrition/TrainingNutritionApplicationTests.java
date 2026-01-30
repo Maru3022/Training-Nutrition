36478f9fa44209b83708886b68292a3731547ea4
Maru <pdijsj2992@gmail.com>
2026-01-30 21:05:47 +0300

    update TrainingNutritionApplicationTests

--- src/test/java/com/example/trainingnutrition/TrainingNutritionApplicationTests.java
+++ src/test/java/com/example/trainingnutrition/TrainingNutritionApplicationTests.java
@@ -1,11 +1,23 @@
 package com.example.trainingnutrition;
 
+import com.example.trainingnutrition.Service.messaging.KafkaProducerService;
+import com.example.trainingnutrition.Service.nutrition.impl.DefaultTipServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(
+        excludeAutoConfiguration = {
+                org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration.class
+        }
+)
 class TrainingNutritionApplicationTests {
+
+    @MockBean
+    private com.example.trainingnutrition.Service.messaging.KafkaProducerService kafkaProducerService;
 
     @Test
     void contextLoads() {}
 }
+
