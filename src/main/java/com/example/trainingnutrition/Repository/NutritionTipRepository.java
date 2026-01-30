f7ce119fae7fadee32954a8fcad26646202e48be
Maru <pdijsj2992@gmail.com>
2026-01-30 14:58:15 +0300

    create KafkaProducerService, update NutritionTipRepository

--- src/main/java/com/example/trainingnutrition/Repository/NutritionTipRepository.java
+++ src/main/java/com/example/trainingnutrition/Repository/NutritionTipRepository.java
@@ -1,4 +1,9 @@
 package com.example.trainingnutrition.Repository;
 
-public class NutritionTipRepository {
+import com.example.trainingnutrition.Domain.NutritionTip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NutritionTipRepository extends JpaRepository<NutritionTip,Long> {
 }
--- src/main/java/com/example/trainingnutrition/Service/messaging/KafkaProducerService.java
+++ src/main/java/com/example/trainingnutrition/Service/messaging/KafkaProducerService.java
@@ -1,4 +1,16 @@
 package com.example.trainingnutrition.Service.messaging;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class KafkaProducerService {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendMessage(String topic, Object message) {
+        System.out.println("Sending message to topic: " + topic);
+        kafkaTemplate.send(topic, message);
+    }
 }
