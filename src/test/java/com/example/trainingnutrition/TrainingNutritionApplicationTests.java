c6ca3b9026dfcc8f67985ebe3cbd64adcd0fa366
Maru <pdijsj2992@gmail.com>
2026-01-30 20:46:15 +0300

    update TrainingNutritionApplicationTests and delete duplicate

--- src/test/java/com/example/trainingnutrition/TrainingNutritionApplicationTests.java
+++ src/test/java/com/example/trainingnutrition/TrainingNutritionApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.trainingnutrition;
 
+import com.example.trainingnutrition.Service.messaging.KafkaProducerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TrainingNutritionApplicationTests {
+
+    @MockBean
+    private KafkaProducerService kafkaProducerService;
 
     @Test
     void contextLoads() {
     }
-
 }
