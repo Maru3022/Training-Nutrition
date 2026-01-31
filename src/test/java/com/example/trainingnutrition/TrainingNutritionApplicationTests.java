771fed644eedaef2e9a85ee3cfd8d659523e29db
Maru <pdijsj2992@gmail.com>
2026-01-31 07:20:21 +0300

    update 3 times pom.xml, TrainingNutritionApplicationTests, application-test.yml

--- src/test/java/com/example/trainingnutrition/TrainingNutritionApplicationTests.java
+++ src/test/java/com/example/trainingnutrition/TrainingNutritionApplicationTests.java
@@ -1,22 +1,17 @@
 package com.example.trainingnutrition;
 
 import com.example.trainingnutrition.Service.messaging.KafkaProducerService;
-import com.example.trainingnutrition.Service.nutrition.impl.DefaultTipServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(properties = {
-        "spring.data.elasticsearch.enabled=false",
-        "spring.kafka.bootstrap-servers=dummy:9092"
-})
+@SpringBootTest
 class TrainingNutritionApplicationTests {
 
-    @MockBean
-    private com.example.trainingnutrition.Service.messaging.KafkaProducerService kafkaProducerService;
+    @MockitoBean
+    private KafkaProducerService kafkaProducerService;
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+    }
 }
-
-
